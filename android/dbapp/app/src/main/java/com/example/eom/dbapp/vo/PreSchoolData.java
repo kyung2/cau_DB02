7e9d095243223fae4bb648f2ffb7b58275832a7e
kyung2 <lovelyhyunkyung@gmail.com>
2017-06-06 15:55:17 +0900

    preSchoolData

--- android/dbapp/app/src/main/java/com/example/eom/dbapp/vo/PreSchoolData.java
+++ android/dbapp/app/src/main/java/com/example/eom/dbapp/vo/PreSchoolData.java
@@ -4,49 +4,71 @@ package com.example.eom.dbapp.vo;
  * Created by hyunkyung on 2017-06-05.
  */
 
+import java.util.Date;
+
 /**
- * <String> 유치원이름,시도,시군구,주소,전화번호
+ * <String> 유치원이름,시도,시군구,주소,전화번호(팩스), 어린이집유형
  *     string name : 유치원 이름
- *     string sido, string sigu 시도,시군구
- *     String tel_num,fax_num;
+ *     string si_do, string si_gun_gu 시도,시군구
+ *     String tel,fax;
+ *     String address :주소
+ *     String assess_certification_type;타입
+ * <Int> 우편번호,놀이터수,보육실수,보육교직원수,현원,정원,총 시설규모
+ *     postcode,playground_num,nursing_room_num,nursing_room_area,capacity,NumofCCTV
+ *     int postcode : 우편번호
+ *     int playground_num,nursing_room_num,nursing_room,area,capacity,NumofCCTV;
+ *
+ *<Double> 위도,경도
+ * Double latitude,longitude 위도 경도
+ * <Boolean>운영현황 통학차량여부
+ * bool operation_state,school_bus;
  *
  *
+ *<Date> 인가일자, 휴지시작일지,휴지종료일자 폐지일자
+ * Permit,pause_S,pause_E,close
  *
- * <Int> 우편번호,놀이터수,보육실수,정원,총 시설규모
- *     int zipcode : 우편번호
- *     i
- *
- *
+ * Date pause_start_date,pause_end_date,close_date,permit_date;
  *
  *
- * <Boolean>
- *
- *
- *<Date>
- *
- name : 키즈카페이름
- id : 번호(고유)
- sido: 시도
- sigun : 시군구
- business_operator_name :사업자명
- zipcode :소새지 우편번호
- address  :소재지지번주소
- autoDate  :인허가일자 (ㅇdate)
- state ㅣ:영업상태명(운영중 TF)
- multiuser  다중 이용 업소 여부 :TF
- facility_size   총 시설규모 (m^2)
- (Remove: ) 위생업조명(일반음식점이냐 아니냐
- (Remove:  위생업태명 (키즈카페)
- double : latitude,longitude  위도경도 double
  **/
 
 
 
 public class PreSchoolData {
-    public String name, address, phone_num,id;
-    public PreSchoolData(String name, String address, String phone_num){
+    public String name, address, si_do, si_gun_gu, tel,fax,assess_certification_type;
+    public int id,postcode,playground_num,nursing_room_num,nursing_room_area,capacity,NumofCCTV;
+    public Date pause_start_date,pause_end_date,close_date,permit_date;
+    public boolean operation_state,school_bus;
+    public double latitude,longitude;
+
+
+    public PreSchoolData(String name, String address,String si_do,String si_gun_gu,String tel ,String fax, String assess_certification_type,
+                         int id, int postcode ,int playground_num , int nursing_room_area , int nursing_room_num, int capacity ,int NumofCCTV,
+                         Date pause_end_date , Date pause_start_date, Date close_date, Date permit_date,
+                         double latitude, double longitude,
+                         boolean operation_state , boolean school_bus) {
         this.name = name;
         this.address = address;
-        this.phone_num = phone_num;
+        this.si_do = si_do;
+        this.si_gun_gu = si_gun_gu;
+        this.tel=tel;
+        this.fax = fax;
+        this.assess_certification_type = assess_certification_type;
+        this.id = id;
+        this.postcode = postcode;
+        this.playground_num = playground_num;
+        this.nursing_room_area = nursing_room_area;
+        this.nursing_room_num = nursing_room_num;
+        this.capacity = capacity;
+        this.NumofCCTV = NumofCCTV;
+        this.pause_end_date = pause_end_date;
+        this.pause_start_date = pause_start_date;
+        this.close_date = close_date;
+        this.permit_date = permit_date;
+        this.latitude =  latitude;
+        this.longitude = longitude;
+        this.operation_state = operation_state;
+        this.school_bus = school_bus;
+
     }
 }
