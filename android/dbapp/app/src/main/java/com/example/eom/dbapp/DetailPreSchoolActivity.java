dd45fa26b3a668614e9242f9926b9ec07c880e45
kyung2 <lovelyhyunkyung@gmail.com>
2017-06-07 02:24:07 +0900

    유치원 상세정보

--- android/dbapp/app/src/main/java/com/example/eom/dbapp/DetailPreSchoolActivity.java
+++ android/dbapp/app/src/main/java/com/example/eom/dbapp/DetailPreSchoolActivity.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 import com.example.eom.dbapp.vo.KidsCafeData;
 import com.example.eom.dbapp.vo.PreSchoolData;
 
+import org.w3c.dom.Text;
+
 /**
  * Created by hyunkyung on 2017-06-05.
  */
@@ -21,24 +23,63 @@ public class DetailPreSchoolActivity extends AppCompatActivity {
     private void setData(PreSchoolData preSchoolData) {
         TextView tv_name = (TextView) findViewById(R.id.tv_detail_pre_school_name);
         tv_name.setText(preSchoolData.name);
+
         TextView tv_permit_date = (TextView) findViewById(R.id.tv_detail_pre_school_permit_date);
         tv_permit_date.setText((CharSequence) preSchoolData.permit_date);
+
+        TextView tv_type = (TextView) findViewById(R.id.tv_detail_pre_school_type);
+        tv_type.setText(preSchoolData.assess_certification_type);
+
         TextView tv_address = (TextView) findViewById(R.id.tv_detail_pre_school_address);
         tv_address.setText(preSchoolData.address);
+
         TextView tv_postcode = (TextView) findViewById(R.id.tv_detail_pre_school_postcode);
         tv_postcode.setText(preSchoolData.postcode);
+
         TextView tv_si_do = (TextView) findViewById(R.id.tv_detail_pre_school_si_do);
         tv_si_do.setText(preSchoolData.si_do);
+
         TextView tv_si_gun_gu = (TextView) findViewById(R.id.tv_detail_pre_school_si_gun_gu);
         tv_si_gun_gu.setText(preSchoolData.si_gun_gu);
+
         TextView tv_operation_state = (TextView) findViewById(R.id.tv_detail_pre_school_close_date);
         tv_operation_state.setText("" + preSchoolData.close_date);
+
+        TextView tv_pause_start_date = (TextView) findViewById(R.id.tv_detail_pre_school_pause_start_date);
+        tv_pause_start_date.setText(""+preSchoolData.pause_start_date);
+
+        TextView tv_pause_end_date = (TextView) findViewById(R.id.tv_detail_pre_school_pause_end_date);
+        tv_pause_end_date.setText(""+preSchoolData.pause_end_date);
+
         TextView tv_latitude = (TextView) findViewById(R.id.tv_detail_pre_school_latitude);
         tv_latitude.setText("" + preSchoolData.latitude);
+
         TextView tv_longitude = (TextView) findViewById(R.id.tv_detail_pre_school_longitude);
         tv_longitude.setText("" + preSchoolData.longitude);
 
+        TextView tv_tel = (TextView) findViewById(R.id.tv_detail_pre_school_tel);
+        tv_tel.setText(preSchoolData.tel);
+
+        TextView tv_fax = (TextView) findViewById(R.id.tv_detail_pre_school_fax);
+        tv_fax.setText(preSchoolData.fax);
+
+        TextView tv_capacity = (TextView) findViewById(R.id.tv_detail_pre_school_capacity);
+        tv_capacity.setText(preSchoolData.fax);
+
+        TextView tv_school_bus = (TextView) findViewById(R.id.tv_detail_pre_school_shcool_bus);
+        tv_school_bus.setText(""+preSchoolData.school_bus);
+
+        TextView tv_playground_num = (TextView) findViewById(R.id.tv_detail_pre_school_playground_num);
+        tv_playground_num.setText(preSchoolData.playground_num);
+
+        TextView tv_nursing_room_area = (TextView) findViewById(R.id.tv_detail_pre_school_nursing_room_area);
+        tv_nursing_room_area.setText(preSchoolData.nursing_room_area);
+
+        TextView tv_nursing_room_num = (TextView) findViewById(R.id.tv_detail_pre_school_nursing_room_num);
+        tv_nursing_room_num.setText(preSchoolData.nursing_room_num);
+
+        TextView tv_NumofCCTV = (TextView) findViewById(R.id.tv_detail_pre_school_NumofCCTV);
+        tv_NumofCCTV.setText(preSchoolData.NumofCCTV);
     }
 }
-
 
