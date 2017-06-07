e8c00fab37b701da3fa2d97f6e48854e866ed8c7
kyung2 <lovelyhyunkyung@gmail.com>
2017-06-08 08:47:58 +0900

    머리머리

---
+++ android/dbapp/app/src/main/java/com/example/eom/dbapp/DetailElmSchoolActivity.java
@@ -0,0 +1,13 @@
+package com.example.eom.dbapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailElmSchoolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_elm_school);
+    }
+}
