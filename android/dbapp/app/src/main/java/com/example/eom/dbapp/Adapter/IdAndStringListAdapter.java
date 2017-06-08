63f83b96ce2e7e701a999e79f68cf110474e0eae
sangwon0001 <sangwon0001@gmail.com>
2017-06-08 11:59:49 +0900

    id 연결

--- android/dbapp/app/src/main/java/com/example/eom/dbapp/Adapter/IdAndStringListAdapter.java
+++ android/dbapp/app/src/main/java/com/example/eom/dbapp/Adapter/IdAndStringListAdapter.java
@@ -2,12 +2,21 @@ package com.example.eom.dbapp.Adapter;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.eom.dbapp.DetailActivity.DetailChildCareCenterActivity;
+import com.example.eom.dbapp.ListActivity.ChildCareCenterListActivity;
+import com.example.eom.dbapp.ListActivity.ElmSchoolListActivity;
+import com.example.eom.dbapp.ListActivity.HospitalListActivity;
+import com.example.eom.dbapp.ListActivity.PlayFacilityListActivity;
+import com.example.eom.dbapp.ListActivity.SafeAreaListActivity;
+import com.example.eom.dbapp.ListActivity.TrafficAccidentAreaListActivity;
+import com.example.eom.dbapp.ListActivity.WalfareServiceListActivity;
 import com.example.eom.dbapp.R;
 import com.example.eom.dbapp.vo.IdAndString;
 
@@ -42,7 +51,26 @@ public class IdAndStringListAdapter extends RecyclerView.Adapter<IdAndStringList
         holder.view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(item.type==IdAndString.ChildCareCenterListActivity){
+                    context.startActivity(new Intent(context,DetailChildCareCenterActivity.class));
+                }else if(item.type==IdAndString.ElmSchoolListActivity){
+                    context.startActivity(new Intent(context,ElmSchoolListActivity.class));
+                }else if(item.type==IdAndString.HospitalListActivity){
+                    context.startActivity(new Intent(context,HospitalListActivity.class));
 
+                }else if(item.type==IdAndString.PlayFacilityListActivity){
+                    context.startActivity(new Intent(context,PlayFacilityListActivity.class));
+
+                }else if(item.type==IdAndString.SafeAreaListActivity){
+                    context.startActivity(new Intent(context,SafeAreaListActivity.class));
+
+                }else if(item.type==IdAndString.TrafficAccidentAreaListActivity){
+                    context.startActivity(new Intent(context,TrafficAccidentAreaListActivity.class));
+
+                }else if(item.type==IdAndString.WalfareServiceListActivity){
+                    context.startActivity(new Intent(context,WalfareServiceListActivity.class));
+
+                }
             }
         });
     }
