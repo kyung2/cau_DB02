23607080e8f899444375dc94af54b1106261a14c
kyung2 <lovelyhyunkyung@gmail.com>
2017-06-08 09:04:42 +0900

    서ㅏㅇ쾌하게머리륽감자!

--- android/dbapp/app/src/main/java/com/example/eom/dbapp/DetailElmSchoolActivity.java
+++ android/dbapp/app/src/main/java/com/example/eom/dbapp/DetailElmSchoolActivity.java
@@ -2,6 +2,10 @@ package com.example.eom.dbapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.eom.dbapp.network.PreSchoolsDetailTask;
+import com.example.eom.dbapp.vo.ElmSchoolData;
 
 public class DetailElmSchoolActivity extends AppCompatActivity {
 
@@ -9,5 +13,52 @@ public class DetailElmSchoolActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_elm_school);
+        new PreSchoolsDetailTask(getIntent().getIntExtra("id",1)){
+            @Override
+            protected void onPostExecute(ElmSchoolData elmSchoolData) {
+                super.onPostExecute(elmSchoolData);
+                setData(elmSchoolData);
+            }
+        }.execute("");
+
+    }
+
+    private void setData(ElmSchoolData elmSchoolData) {
+        TextView tv_big_name = (TextView) findViewById(R.id.tv_detail_elm_school_big_name);
+        tv_big_name.setText(elmSchoolData.name);
+
+        TextView tv_name = (TextView) findViewById(R.id.tv_detail_elm_school_name);
+        tv_name.setText(elmSchoolData.name);
+
+        TextView tv_tel = (TextView) findViewById(R.id.tv_detail_elm_school_tel);
+        tv_tel.setText(elmSchoolData.tel);
+
+
+        TextView tv_postcode = (TextView) findViewById(R.id.tv_detail_elm_school_postcode);
+        tv_postcode.setText(""+elmSchoolData.postcode);
+
+        TextView tv_si_do = (TextView) findViewById(R.id.tv_detail_elm_school_si_do);
+        tv_si_do.setText(elmSchoolData.si_do);
+
+        TextView tv_si_gun_gu = (TextView) findViewById(R.id.tv_detail_elm_school_si_gun_gu);
+        tv_si_gun_gu.setText(elmSchoolData.si_gun_gu);
+
+        TextView tv_address = (TextView) findViewById(R.id.tv_detail_elm_school_address);
+        tv_address.setText(elmSchoolData.address);
+
+        TextView tv_homepage = (TextView) findViewById(R.id.tv_detail_elm_school_homepage);
+        tv_homepage.setText(elmSchoolData.homepage);
+
+        TextView tv_public_private = (TextView) findViewById(R.id.tv_detail_elm_school_public_private);
+        tv_public_private.setText("" + elmSchoolData.public_private);
+
+        TextView tv_latitude = (TextView) findViewById(R.id.tv_detail_elm_school_latitude);
+        tv_latitude.setText("" + elmSchoolData.latitude);
+
+        TextView tv_longitude = (TextView) findViewById(R.id.tv_detail_elm_school_longitude);
+        tv_longitude.setText("" + elmSchoolData.longitude);
+
+
     }
 }
+
