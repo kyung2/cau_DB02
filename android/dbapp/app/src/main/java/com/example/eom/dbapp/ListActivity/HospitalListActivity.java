b0da26ecd60ddb71a02fa038ff5bcc6b1d5f5490
kyung2 <lovelyhyunkyung@gmail.com>
2017-06-08 23:38:32 +0900

    push

--- android/dbapp/app/src/main/java/com/example/eom/dbapp/ListActivity/HospitalListActivity.java
+++ android/dbapp/app/src/main/java/com/example/eom/dbapp/ListActivity/HospitalListActivity.java
@@ -9,7 +9,7 @@ import android.support.v7.widget.RecyclerView;
 
 import com.example.eom.dbapp.Adapter.IdAndStringListAdapter;
 import com.example.eom.dbapp.Adapter.KidsCenterAdpater;
-import com.example.eom.dbapp.R;
+import com.example.eom.dbapp.DetailActivity.R;
 import com.example.eom.dbapp.network.KidsCenterByGPSTask;
 import com.example.eom.dbapp.network.ListByGPSTask;
 import com.example.eom.dbapp.vo.IdAndString;
@@ -25,16 +25,13 @@ public class HospitalListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kidscenter_list);
-
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.rv_kidscenter);
         final ArrayList<IdAndString> arrayList = new ArrayList<>();
         RecyclerView.ItemDecoration itemDecoration =
                 new DividerItemDecoration(this, LinearLayoutManager.VERTICAL);
-        recyclerView.addItemDecoration(itemDecoration);
+
 
         adapter = new IdAndStringListAdapter(this,arrayList);
-        recyclerView.setAdapter(adapter);
+        //  recyclerView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
 
         new ListByGPSTask(this,"/hospital/gps/","hospital"){
