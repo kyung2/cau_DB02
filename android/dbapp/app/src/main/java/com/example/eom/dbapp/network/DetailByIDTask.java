092071a600888a36bdc2f80879bb01815b70391e
sangwon0001 <sangwon0001@gmail.com>
2017-06-08 11:36:31 +0900

    여러 페이지 만듬

--- android/dbapp/app/src/main/java/com/example/eom/dbapp/network/DetailByIDTask.java
+++ android/dbapp/app/src/main/java/com/example/eom/dbapp/network/DetailByIDTask.java
@@ -6,7 +6,6 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import com.example.eom.dbapp.MySharedPreferences;
-import com.example.eom.dbapp.vo.KidsCenterData;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -14,7 +13,6 @@ import org.json.JSONObject;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 
 import static com.example.eom.dbapp.network.NetworkManager.setupConnection;
 
@@ -22,31 +20,27 @@ import static com.example.eom.dbapp.network.NetworkManager.setupConnection;
  * Created by dhtpr on 2017-06-08.
  */
 
-public abstract class ListByGPSTask extends AsyncTask<String, Void,JSONArray> {
-    //data={"user_id":19,"longitude":126.95,"latitude":37.50,"range":1}37.50,126.95
-    private double latitude,longitude;
+public abstract class DetailByIDTask extends AsyncTask<String, Void,JSONObject> {
     String murl,parsestring;
+    int id;
 
-    protected ListByGPSTask(Context context, String url,String parsestring) {
-        SharedPreferences sp = context.getSharedPreferences(MySharedPreferences.NAME,0);
-        latitude = Double.parseDouble(sp.getString(MySharedPreferences.USER_LATITUDE,"35"));
-        longitude = Double.parseDouble(sp.getString(MySharedPreferences.USER_LONGITUDE,"123"));
+    protected DetailByIDTask(int id, String url, String parsestring) {
         this.murl=url;
         this.parsestring=parsestring;
+        this.id=id;
     }
 
     @Override
-    protected JSONArray doInBackground(String... params) {
+    protected JSONObject doInBackground(String... params) {
         InputStream inputStream;
         String strResult = "";
         String result = null;
-        JSONArray items = new JSONArray();
+        JSONObject item = new JSONObject();
         try {
             URL url = new URL(NetworkManager.serverIP + murl);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("latitude", latitude);
-            jsonObject.put("longitude", longitude);
+            jsonObject.put("id", id);
             String strJson = "data=" + jsonObject.toString();
             Log.d("parsestring", strJson);
             setupConnection(strJson, urlConnection);
@@ -57,12 +51,11 @@ public abstract class ListByGPSTask extends AsyncTask<String, Void,JSONArray> {
             else
                 strResult = "Did not work!";
             Log.d("parsestring", strResult);
-            items = new JSONObject(strResult).getJSONArray(parsestring);
-            return items;
+            item = new JSONArray(new JSONObject(strResult).getString(parsestring)).getJSONObject(0).getJSONObject("fields");
+            return item;
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        return items;
+        return item;
     }
 }
